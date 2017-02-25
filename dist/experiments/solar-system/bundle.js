4d7abe2708d698b5aea7e597b74e47e23f1e2c3b
SalamAlmajdi <salamalmajdi@gmail.com>
2017-02-26 09:56:48 +1100

    build

--- dist/experiments/solar-system/bundle.js
+++ dist/experiments/solar-system/bundle.js
@@ -50,6 +50,14 @@
 
 	var _Planet2 = _interopRequireDefault(_Planet);
 
+	var _Star = __webpack_require__(2);
+
+	var _Star2 = _interopRequireDefault(_Star);
+
+	var _entities = __webpack_require__(3);
+
+	var _entities2 = _interopRequireDefault(_entities);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var SCREEN_WIDTH = window.innerWidth;
@@ -62,91 +70,33 @@
 	var scene = void 0;
 	var camera = void 0;
 	var renderer = void 0;
-	var sunGeometry = void 0;
-	var sunMaterial = void 0;
 	var sun = void 0;
 	var controls = void 0;
 	var ambientLight = void 0;
 
-	function randomAngle() {
-	  return Math.random() * Math.PI * 2;
-	}
-
-	// or
-	//
-	// const randomAngle = () => {
-	//   return Math.random() * Math.PI * 2;
-	// }
-	//
-	// or
-	//
-	// const randomAngle = () => Math.random() * Math.PI * 2;
-
 	var planets = [];
-	var planetProps = [];
-
-	planetProps.push({
-	  name: 'mercury',
-	  orbitRadius: 200,
-	  radius: 10,
-	  speed: Math.PI * 2 / (360 * 2),
-	  angle: randomAngle(),
-	  color: 0x666666
-	});
-
-	planetProps.push({
-	  name: 'venus',
-	  orbitRadius: 300,
-	  radius: 20,
-	  speed: Math.PI * 2 / (360 * 3),
-	  angle: randomAngle(),
-	  color: 0xffee00
-	});
-
-	planetProps.push({
-	  name: 'earth',
-	  orbitRadius: 400,
-	  radius: 40,
-	  speed: Math.PI * 2 / (360 * 4),
-	  angle: randomAngle(),
-	  // color: 0x0000ff,
-	  texture: '../../assets/textures/planets/earth.jpg'
-	});
-
-	planetProps.push({
-	  name: 'mars',
-	  orbitRadius: 600,
-	  radius: 30,
-	  speed: Math.PI * 2 / (360 * 5),
-	  angle: randomAngle(),
-	  color: 0xff0000
-	});
-
-	var origin = new THREE.Vector3(0, 0, 0);
 
 	function init() {
 	  scene = new THREE.Scene();
 
 	  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-	  camera.position.set(0, 0, 3000);
-	  camera.lookAt(origin);
+	  camera.position.set(0, 1000, 1200);
 
-	  var axisHelper = new THREE.AxisHelper(500);
-	  scene.add(axisHelper);
+	  sun = new _Star2.default({
+	    radius: 100,
+	    color: 0xffffff,
+	    texture: '../../assets/textures/planets/sun.jpg'
+	  });
 
-	  sunGeometry = new THREE.SphereBufferGeometry(100, 32, 32);
-	  sunMaterial = new THREE.MeshLambertMaterial();
-
-	  sun = new THREE.Mesh(sunGeometry, sunMaterial);
 	  scene.add(sun);
 
-	  planetProps.forEach(function (props) {
+	  _entities2.default.forEach(function (props) {
 	    var planet = new _Planet2.default(props);
 	    planets.push(planet);
 	    scene.add(planet);
 	  });
 
-	  ambientLight = new THREE.AmbientLight(0xffffff, 1);
+	  ambientLight = new THREE.AmbientLight(0xffffff, 2);
 	  scene.add(ambientLight);
 
 	  renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -157,30 +107,6 @@
 	  THREEx.WindowResize(renderer, camera);
 
 	  document.body.appendChild(renderer.domElement);
-
-	  var loader = new THREE.TextureLoader();
-
-	  // // load a resource
-	  // loader.load(
-	  //   // resource URL
-	  //   '../../assets/textures/planets/earth.jpg',
-	  //   // Function when resource is loaded
-	  //   (texture) => {
-	  //     console.log(texture);
-	  //     earthMaterial.map = texture;
-	  //     earthMaterial.needsUpdate = true;
-	  //   },
-	  // );
-
-	  // load a resource
-	  loader.load(
-	  // resource URL
-	  '../../assets/textures/planets/sun.jpg',
-	  // Function when resource is loaded
-	  function (texture) {
-	    sunMaterial.map = texture;
-	    sunMaterial.needsUpdate = true;
-	  });
 	}
 
 	function update() {
@@ -257,6 +183,7 @@
 	    // removes the line from the center of the circle to the edge of the circle
 	    var orbitMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 	    var orbitMesh = new THREE.Line(orbitGeometry, orbitMaterial);
+	    orbitMesh.rotation.x = Math.PI / 2;
 	    _this.orbitMesh = orbitMesh;
 	    _this.add(orbitMesh);
 	    return _this;
@@ -266,10 +193,10 @@
 	    key: "update",
 	    value: function update() {
 	      var x = this.props.orbitRadius * Math.cos(this.props.angle);
-	      var y = this.props.orbitRadius * Math.sin(this.props.angle);
+	      var z = this.props.orbitRadius * Math.sin(this.props.angle);
 	      this.props.angle += this.props.speed;
-	      this.position.set(x, y, 0);
-	      this.orbitMesh.position.set(-x, -y, 0);
+	      this.position.set(x, 0, z);
+	      this.orbitMesh.position.set(-x, 0, -z);
 	    }
 	  }]);
 
@@ -277,6 +204,143 @@
 	}(THREE.Object3D);
 
 	exports.default = Planet;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var loader = new THREE.TextureLoader();
+
+	var Star = function (_THREE$Object3D) {
+	  _inherits(Star, _THREE$Object3D);
+
+	  function Star(props) {
+	    _classCallCheck(this, Star);
+
+	    var _this = _possibleConstructorReturn(this, (Star.__proto__ || Object.getPrototypeOf(Star)).call(this));
+
+	    _this.props = props;
+
+	    _this.planets = [];
+
+	    var geometry = new THREE.SphereBufferGeometry(props.radius, 32, 32);
+	    var material = new THREE.MeshStandardMaterial({ color: props.color });
+	    var mesh = new THREE.Mesh(geometry, material);
+	    _this.add(mesh);
+
+	    if (props.texture) {
+	      loader.load(
+	      // resource URL
+	      props.texture,
+	      // Function when resource is loaded
+	      function (texture) {
+	        material.map = texture;
+	        material.needsUpdate = true;
+	      });
+	    }
+
+	    // // create orbit path
+	    // const orbitGeometry = new THREE.CircleGeometry(props.orbitRadius, 64);
+	    // orbitGeometry.vertices.shift();
+	    // // removes the line from the center of the circle to the edge of the circle
+	    // const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+	    // const orbitMesh = new THREE.Line(orbitGeometry, orbitMaterial);
+	    // orbitMesh.rotation.x = Math.PI / 2;
+	    // this.orbitMesh = orbitMesh;
+	    // this.add(orbitMesh);
+	    return _this;
+	  }
+
+	  _createClass(Star, [{
+	    key: "addPlanet",
+	    value: function addPlanet(planet) {
+	      this.planets.push(planet);
+	    }
+	  }]);
+
+	  return Star;
+	}(THREE.Object3D);
+
+	exports.default = Star;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	function randomAngle() {
+	  return Math.random() * Math.PI * 2;
+	}
+
+	// or
+	//
+	// const randomAngle = () => {
+	//   return Math.random() * Math.PI * 2;
+	// }
+	//
+	// or
+	//
+	// const randomAngle = () => Math.random() * Math.PI * 2;
+
+
+	var planets = [];
+
+	planets.push({
+	  name: 'mercury',
+	  orbitRadius: 200,
+	  radius: 10,
+	  speed: Math.PI * 2 / (360 * 2),
+	  angle: randomAngle(),
+	  color: 0x666666
+	});
+
+	planets.push({
+	  name: 'venus',
+	  orbitRadius: 300,
+	  radius: 20,
+	  speed: Math.PI * 2 / (360 * 3),
+	  angle: randomAngle(),
+	  color: 0xffee00
+	});
+
+	planets.push({
+	  name: 'earth',
+	  orbitRadius: 400,
+	  radius: 40,
+	  speed: Math.PI * 2 / (360 * 4),
+	  angle: randomAngle(),
+	  // color: 0x0000ff,
+	  texture: '../../assets/textures/planets/earth.jpg'
+	});
+
+	planets.push({
+	  name: 'mars',
+	  orbitRadius: 600,
+	  radius: 30,
+	  speed: Math.PI * 2 / (360 * 5),
+	  angle: randomAngle(),
+	  color: 0xff0000
+	});
+
+	exports.default = planets;
 
 /***/ }
 /******/ ]);
