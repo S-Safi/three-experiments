8bab53373f2305cf22eefd7ca85e529498f31cf6
Salam Safi <salamalmajdi@gmail.com>
2018-04-22 08:38:42 +1000

    Update to latest version.

--- src/experiments/hello-world/config.js
+++ src/experiments/hello-world/config.js
@@ -4,8 +4,8 @@ export default {
   tags: 'Cube',
   public: true,
   scripts: [
-    '../../libs/three/r83/three.min.js',
-    '../../libs/three/r83/controls/OrbitControls.js',
+    '../../libs/three/r92/three.min.js',
+    '../../libs/three/r92/controls/OrbitControls.js',
     '../../libs/stats/r17/stats.min.js',
     '../../libs/threex/THREEx.WindowResize.js',
     '../../libs/dat.gui/0.6.2/dat.gui.min.js',
