19e4069832b9d43eb2eb820c7e3741525542d52d
Salam Safi <salamalmajdi@gmail.com>
2018-07-15 08:20:47 +1000

    Start objectmove experiment

---
+++ src/experiments/object-move/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'Object Move',
+  description: 'Experiment to move an object with the keyboard',
+  public: true,
+  scripts: [
+    '../../libs/three/r83/three.min.js',
+    '../../libs/three/r83/controls/OrbitControls.js',
+    '../../libs/threex/THREEx.WindowResize.js',
+    '../../libs/stemkoski/KeyboardState.js',
+  ],
+  styles: [],
+};
