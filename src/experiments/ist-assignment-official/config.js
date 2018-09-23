68d8f513c700d5b8aef459f6cf857c8d7dae1d73
Salam Safi <salamalmajdi@gmail.com>
2018-09-23 21:34:06 +1000

    Added official ist assignment

---
+++ src/experiments/ist-assignment-official/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'The ist assignment',
+  description: 'Click here Mr sinclair.',
+  tags: 'Cube',
+  public: true,
+  scripts: [
+    '../../libs/three/r92/three.min.js',
+    '../../libs/three/r92/controls/OrbitControls.js',
+    '../../libs/stats/r17/stats.min.js',
+    '../../libs/threex/THREEx.WindowResize.js',
+    '../../libs/dat.gui/0.6.2/dat.gui.min.js',
+    '../../libs/stemkoski/KeyboardState.js',
+  ],
+  styles: [
+    '../../libs/dat.gui/0.6.2/dat.gui.css',
+  ],
+};
